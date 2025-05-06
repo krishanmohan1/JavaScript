b7c95895368b92d5221eb5a189230c03b5f15154
Krishan Mohan <krishanmohan3501@gmail.com>
2025-05-07 00:00:02 +0530

    first commit

---
+++ 05_iterations/three.js
@@ -0,0 +1,48 @@
+// [" "," "," "]
+
+// [{},{},{}]
+
+// for of loop
+// array ,string , object all are iterable
+
+const arr = [1, 2, 3, 4, 5];
+
+for (const num of arr) {
+  //console.log(`numbers are ${num}`);
+}
+
+const greetings = "Hello World!";
+for (const greet of greetings) {
+  if (greet == " ") {
+    continue;
+  }
+//   console.log(`Each char is : ${greet}`);
+}
+
+
+
+// Maps
+
+const map1 = new Map();
+map1.set("a", 1);
+map1.set("b",2);
+map1.set("c",3);
+
+// console.log(map1)
+for (const map of map1) {
+    // console.log(map);
+}
+
+
+const map = new Map()
+map.set('In', 'India')
+map.set('USA', 'United State of America')
+map.set('Fr','France')
+
+// console.log(map);
+for (const [key,value] of map) {
+    // console.log(key);   // it will print in key and value pair array of each entry
+    console.log(key ,':-' , value);   
+}
+
+// the way we iterate on map , we cannot iterate on 
---
+++ 05_iterations/two.js
@@ -0,0 +1,23 @@
+let i = 0;
+
+while (i <= 10) {
+  console.log(`value is ${i}`);
+  i++;
+}
+
+let arr = 0;
+let myArray = ["flash", "batman", "superman"];
+while (arr < myArray.length) {
+  console.log(`value is ${myArray[arr]}`);
+  arr++;
+}
+
+
+
+let score = 1
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+    
+} while (score<=10);
