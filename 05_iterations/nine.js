2c0464a037fc7100e1a3635dca4c4269039cdaa5
Krishan Mohan <krishanmohan3501@gmail.com>
2025-05-18 15:06:48 +0530

    first commit

--- 05_iterations/nine.js
+++ 05_iterations/nine.js
@@ -1,31 +1,30 @@
-const myNums = [1,2,3]
+const myNums = [1, 2, 3];
 
 // const sum = myNums.reduce( function(acc , currvalue) => {
-//     return acc + currvalue 
+//     return acc + currvalue
 // }, 0)
 
-const sum = myNums.reduce( (acc, curr)=> (acc + curr),1)
+const sum = myNums.reduce((acc, curr) => acc + curr, 1);
 console.log(sum);
 
-// this is how you use reduce method , mostly jab sum karna ho cart me to 
-
+// this is how you use reduce method , mostly jab sum karna ho cart me to
 
 const shoppingCart = [
-    {
-        itemName : "js course",
-        price : 2999
-    },
-    {
-        itemName : "py course",
-        price : 999
-    },
-    {
-        itemName : "java course",
-        price : 4999
-    },
-] 
+  {
+    itemName: "js course",
+    price: 2999,
+  },
+  {
+    itemName: "py course",
+    price: 999,
+  },
+  {
+    itemName: "java course",
+    price: 4999,
+  },
+];
 
-// now we have to add the total price of this card , we can use any loop but reduce is best for it 
+// now we have to add the total price of this card , we can use any loop but reduce is best for it
 
-const priceToPay = shoppingCart.reduce( (acc , item)=>(acc + item.price),0)
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 1);
 console.log(`priceToPay for this three courses is : ${priceToPay} `);
