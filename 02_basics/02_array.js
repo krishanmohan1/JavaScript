95ac1f3a702ebdfc428d8aaf181ff92eb9a3d7ef
Krishan Mohan <krishanmohan3501@gmail.com>
2025-05-19 14:07:53 +0530

    first commit

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -29,7 +29,7 @@ console.log(Array.from({name : "Mohan"}));  // interesting , you have to mention
 
 let score1 = 100
 let score2 = 200
-let score3 = 300
+let score3 = 301
 
 console.log(Array.of(score1,score2,score3));
 
